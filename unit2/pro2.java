1813bb453626fcd3b5249edcce21a937eef83bd0
desaisamiksha <146056929+desaisamiksha@users.noreply.github.com>
2024-03-11 09:57:13 +0530

    Add files via upload

--- unit2/pro2.java
+++ unit2/pro2.java
@@ -1,1 +1,26 @@
+import java.io.*;
+import java.net.*;
+public class u2p2
+{
+public static void main(String args[])throws Exception
+{
+try
+{
+System.out.println("Enter website address(for eg:www.google.com):");
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+String str=br.readLine();
+InetAddress ip=InetAddress.getByName(str);
 
+System.out.println("Host Name :"+ip.getHostName());
+System.out.println("IP Address :"+ip.getHostAddress());
+}
+catch(UnknownHostException ex)
+{
+System.out.println("Error :"+ex);
+}
+catch(Exception e)
+{
+System.out.println("Error :"+e);
+}
+}
+}
